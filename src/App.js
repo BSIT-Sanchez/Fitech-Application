c493a33e89b40edd8cdd0d10fce1d45402a44b6a
unknown <sanchezlando@gmail.com>
2024-03-02 11:28:26 +0800

    image

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import HomeScreen from './Screens/HomeScreen'
 import Exercise from './Screens/Exercise'
 import AboutUs from './Screens/AboutUs'
 import ContactUs from './Screens/ContactUs'
 import SignUp from './Screens/SignUp'
+import GetStarted from './Screens/GetStarted'
 
 const App = () => {
   return(
     <Routes>
-      <Route path='/' element={<HomeScreen/>}/>
+      <Route path='/' element={<GetStarted/>}/>
       <Route path='/exercise' element={<Exercise/>}/>
       <Route path='/about-us' element={<AboutUs/>}/>
       <Route path='/contact-us' element={<ContactUs/>}/>
---
+++ src/Screens/GetStarted.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GetStarted = () => {
+  return(
+    <div className='get-container'>
+      <img src="images/image1.png" alt='image1'/>
+    </div>
+  )
+  
+}
+export default GetStarted
