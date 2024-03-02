4520b101556ad9c2980cc7492db9f0e595a28d5a
unknown <sanchezlando@gmail.com>
2024-03-02 11:51:22 +0800

    send

--- src/Screens/GetStarted.js
+++ src/Screens/GetStarted.js
@@ -2,8 +2,21 @@ import React from 'react'
 
 const GetStarted = () => {
   return(
-    <div className='get-container'>
-      <img src="images/image1.png" alt='image1'/>
+    <div className='login-container'>
+      <div className='flex-container'>
+          <div className='image-div'>
+            <img src="images/image1.png" alt='image1' className='image1'/>
+          </div>
+
+          <form>
+            <h3>Login</h3>
+            <label>Email</label>
+            <input type="email" placeholder="admin@gmail.com"/>
+            <label>Password</label>
+            <input type="password" placeholder='1234'/>
+            <button type='submit' value='Login'/>
+          </form>  
+      </div>   
     </div>
   )
   
